7471169b6f8381738aef470abc2e38568ed930ff
张晓辉 <cococandoit@163.com>
2018-12-29 18:54:19 +0800

    add floor state

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
     <h1 class="header"><span class="glb-for-screen">微羽物业大数据运营平台</span></h1>
     <ul class="layout-tr">
       <li>
-          
+          <!-- <left-side></left-side> -->
       </li>
       <li class="layout-td">
           <model-center></model-center>
@@ -17,7 +17,10 @@
       components:{
          modelCenter:function(){
              return import('@/components/center.vue')
-         }  
+         },
+        //  leftSide:function(){
+        //      return import('@/components/left-side.vue')
+        //  }  
       }
   }
 
