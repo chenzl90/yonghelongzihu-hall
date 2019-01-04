d0a507e3eedff5527e7cf525d885bfa21a9a275b
陈占乐 <chenzl765@163.com>
2019-01-04 14:22:04 +0800

    Add files via upload

--- src/components/left-side.vue
+++ src/components/left-side.vue
@@ -40,7 +40,7 @@
               </div>
             </div>
           </div>
-          <div class="finance-progress finance-progress-month"></div>
+          <div class="finance-progress finance-progress-all"></div>
         </section>
       </section>
       <div class="sub-title">月度收费统计</div>
@@ -67,9 +67,9 @@
 
     <section class="wrapper">
       <div class="title">停车场统计</div>
-      <section class="park-wrapper">
-        <section class="park-left">
-          <section class="income income-all">
+      <section class="income-wrapper">
+        <section class="income">
+          <section class="income-all">
             <div class="sub-title">总营收</div>
             <div class="income-park">
               200,000
@@ -80,10 +80,10 @@
               <span class="income-all-color">2000 元</span>
             </div>
           </section>
-          <section class="income income-today">
+          <section class="income-today">
             <div class="sub-title">
               当日营收
-              <span class="little-title">(和昨日同时段的同比)</span>
+              <span class="little-title txt-font14">(和昨日同时段的同比)</span>
             </div>
             <div class="income-park">
               200,000
@@ -91,30 +91,31 @@
             </div>
             <div class="income-increase">+100（+0.42%）</div>
           </section>
-          <section id="park-space"></section>
         </section>
-        <section class="park-right">
-          <section id="income"></section>
-          <section class="park-space">
-            <div class="park-title">
-              <span class="txt-font14">当前空余车位</span>
-              <span class="txt-font52">1200</span>
-              <span class="txt-font20">/ 3200</span>
+
+        <section id="income"></section>
+      </section>
+      <section class="park">
+        <section id="park-space"></section>
+        <section class="park-space">
+          <div class="park-title">
+            <span class="txt-font14">当前空余车位</span>
+            <span class="txt-font52">1200</span>
+            <span class="txt-font20">/ 3200</span>
+          </div>
+          <section class="car-flow">
+            <div class="flow-today">
+              <span class="txt-font16">今日车流量</span>
+              <span class="txt-font32">3200</span>
             </div>
-            <section class="car-flow">
-              <div class="flow-today">
-                <span class="txt-font16">今日车流量</span>
-                <span class="txt-font32">3200</span>
-              </div>
-              <div class="flow-progress flow-today-progress"></div>
-            </section>
-            <section class="car-flow">
-              <div class="flow-average">
-                <span class="txt-font16">日均车流量</span>
-                <span class="txt-font32">3200</span>
-              </div>
-              <div class="flow-progress flow-today-progress"></div>
-            </section>
+            <div class="flow-progress flow-progress-today"></div>
+          </section>
+          <section class="car-flow">
+            <div class="flow-average">
+              <span class="txt-font16">日均车流量</span>
+              <span class="txt-font32">2000</span>
+            </div>
+            <div class="flow-progress flow-progress-average"></div>
           </section>
         </section>
       </section>
@@ -122,6 +123,72 @@
 
     <section class="wrapper">
       <div class="title">访客统计</div>
+      <section class="visitor">
+        <div class="sub-title">访客人数统计</div>
+        <div class="frame txt-font24">
+          本天访问人数
+          <span class="txt-font40">3,000</span>
+        </div>
+        <div class="frame txt-font24">
+          昨日访问人数
+          <span class="txt-font40">3,000</span>
+        </div>
+        <div class="frame txt-font24">
+          本月访问人数
+          <span class="txt-font40">30,000</span>
+        </div>
+        <div class="frame txt-font24">
+          访问总人次
+          <span class="txt-font40">300,000</span>
+        </div>
+      </section>
+      <section class="visitor">
+        <section class="user">
+          <div class="sub-title">用户进出统计</div>
+          <section id="user"></section>
+        </section>
+        <section class="gate">
+          <div class="sub-title">闸机情况统计</div>
+          <div class="gate-wrapper">
+            <table class="gate-table">
+              <thead>
+                <tr class="txt-font16">
+                  <th>序号</th>
+                  <th>设备名称</th>
+                  <th>出入口名称</th>
+                  <th>失败次数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="txt-font16">
+                  <td class="serial-num">01</td>
+                  <td class="device-name">展厅进门左侧1号</td>
+                  <td>展厅左进1</td>
+                  <td class="fail-num">300</td>
+                </tr>
+                <tr class="txt-font16">
+                  <td class="serial-num">02</td>
+                  <td class="device-name">展厅进门左侧2号</td>
+                  <td>展厅左进2</td>
+                  <td class="fail-num">200</td>
+                </tr>
+                <tr class="txt-font16">
+                  <td class="serial-num">03</td>
+                  <td class="device-name">展厅进门右侧1号</td>
+                  <td>展厅右进1</td>
+                  <td class="fail-num">400</td>
+                </tr>
+                <tr class="txt-font16">
+                  <td class="serial-num">04</td>
+                  <td class="device-name">展厅进门右侧2号</td>
+                  <td>展厅右进2</td>
+                  <td class="fail-num">100</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </section>
     </section>
   </div>
 </template>
@@ -139,6 +206,7 @@ export default {
     this.drawOrder();
     this.drawPark();
     this.drawIncome();
+    this.drawUser();
   },
   methods: {
     // 月度收费统计折线图
@@ -150,7 +218,8 @@ export default {
         tooltip: { trigger: "axis" },
         grid: {
           left: "5%",
-          bottom: "15%",
+          top: "4%",
+          bottom: "30%",
           right: "0%"
         },
         legend: {
@@ -234,7 +303,7 @@ export default {
           x: "right",
           y: "center",
           itemGap: 58,
-          left: 240,
+          left: 300,
           data: [
             { name: "正常完成", icon: "circle" },
             { name: "超时完成", icon: "circle" },
@@ -247,8 +316,8 @@ export default {
           {
             name: "访问来源",
             type: "pie",
-            radius: ["50%", "70%"],
-            center: ["20%", "50%"],
+            radius: ["60%", "96%"],
+            center: ["24%", "50%"],
             roseType: "radius",
             avoidLabelOverlap: false,
             label: { normal: { show: false }, emphasis: { show: true } },
@@ -272,7 +341,7 @@ export default {
           x: "right",
           y: "center",
           itemGap: 58,
-          left: 240,
+          left: 300,
           data: [
             { name: "投诉次数", icon: "circle" },
             { name: "报修次数", icon: "circle" }
@@ -284,15 +353,15 @@ export default {
           {
             name: "访问来源",
             type: "pie",
-            radius: ["50%", "70%"],
-            center: ["20%", "50%"],
+            radius: ["60%", "96%"],
+            center: ["24%", "50%"],
             roseType: "radius",
             avoidLabelOverlap: false,
             label: { normal: { show: false }, emphasis: { show: true } },
             lableLine: { normal: { show: false }, emphasis: { show: true } },
             data: [
               { value: 56, name: "投诉次数", itemStyle: { color: "#6694ff" } },
-              { value: 44, name: "保修次数", itemStyle: { color: "#3d4966" } }
+              { value: 44, name: "报修次数", itemStyle: { color: "#3d4966" } }
             ]
           }
         ]
@@ -312,6 +381,7 @@ export default {
         xAxis: {
           type: "value",
           position: "top",
+          min: 10,
           max: 1500,
           axisLine: {
             symbol: ["none", "arrow"],
@@ -324,6 +394,7 @@ export default {
         yAxis: {
           type: "category",
           boundaryGap: true,
+          offset: 2,
           axisLine: {
             symbol: ["arrow", "none"],
             lineStyle: { color: "#0FEBFF" }
@@ -344,7 +415,14 @@ export default {
                 ])
               }
             },
-            label: { show: true, position: "right", distance: 10 },
+            label: {
+              show: true,
+              position: "right",
+              distance: 10,
+              color: "#FFFFFF",
+              fontSize: 18,
+              // formatter: "{value}件"
+            },
             barWidth: 18,
             data: [324, 720, 900, 324]
           }
@@ -357,9 +435,9 @@ export default {
 
       parkGraph.setOption({
         grid: {
-          top: "0%",
           left: "0%",
-          right: "0%"
+          right: "0%",
+          bottom: "4%"
         },
         series: [
           {
@@ -393,6 +471,7 @@ export default {
       let incomeGraph = this.$echarts.init(document.getElementById("income"));
 
       incomeGraph.setOption({
+        grid: { top: "8%", right: "4%" },
         xAxis: {
           type: "category",
           axisLine: {
@@ -430,12 +509,61 @@ export default {
                 ])
               }
             },
-            barWidth: 18
+            barWidth: 18,
+            label: {
+              show: true,
+              position: "top",
+              distance: 10,
+              color: "#FFFFFF",
+              fontSize: 18,
+              // formatter: "{value}元"
+            }
+          }
+        ]
+      });
+    },
+    //
+    drawUser() {
+      let userGraph = this.$echarts.init(document.getElementById("user"));
+      userGraph.setOption({
+        legend: {
+          orient: "vertical",
+          align: "left",
+          x: "right",
+          y: "center",
+          itemGap: 34,
+          left: 280,
+          data: [
+            { name: "物业人员", icon: "circle" },
+            { name: "住户", icon: "circle" },
+            { name: "商户", icon: "circle" },
+            { name: "临时访客", icon: "circle" },
+            { name: "其他", icon: "circle" }
+          ],
+          itemWidth: 14,
+          textStyle: { color: "#FEFEFE", fontSize: 16, padding: [0, 12] }
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["60%", "90%"],
+            center: ["20%", "50%"],
+            roseType: "radius",
+            avoidLabelOverlap: false,
+            label: { normal: { show: false }, emphasis: { show: true } },
+            lableLine: { normal: { show: false }, emphasis: { show: true } },
+            data: [
+              { value: 42, name: "物业人员", itemStyle: { color: "#6694FF" } },
+              { value: 20, name: "住户", itemStyle: { color: "#596A94" } },
+              { value: 20, name: "商户", itemStyle: { color: "#2667FF" } },
+              { value: 8, name: "临时访客", itemStyle: { color: "#2097E3" } },
+              { value: 10, name: "其他", itemStyle: { color: "#1F5E99" } }
+            ]
           }
         ]
       });
     }
-    //
   }
 };
 </script>
@@ -447,13 +575,58 @@ export default {
   width: 2756px;
   margin-left: 60px;
 }
+.txt-font14 {
+  font-size: 14px;
+  font-weight: 400;
+  color: #abb7cc;
+  line-height: 14px;
+}
+.txt-font16 {
+  font-size: 16px;
+  font-weight: 400;
+  color: #abb7cc;
+  line-height: 16px;
+}
+.txt-font20 {
+  font-size: 20px;
+  font-weight: 400;
+  color: #ffffff;
+  line-height: 20px;
+}
+.txt-font24 {
+  font-size: 24px;
+  font-weight: 500;
+  color: #ffffff;
+  line-height: 24px;
+}
+.txt-font32 {
+  font-size: 32px;
+  font-weight: bold;
+  color: #ffffff;
+  line-height: 32px;
+}
+.txt-font40 {
+  font-size: 40px;
+  font-weight: bold;
+  color: #0fe4f8;
+  line-height: 40px;
+}
+.txt-font52 {
+  font-size: 52px;
+  font-weight: 800;
+  color: #0fe4f8;
+  line-height: 52px;
+}
 .wrapper {
-  width: 1306px;
+  width: 1318px;
+  height: 965px;
   padding-left: 80px;
   padding-right: 90px;
+  padding-top: 84px;
+  margin-top: 24px;
   margin-right: 42px;
-  background: url(static/images/left-bg.png) no-repeat 100%;
-  background-size: cover;
+  background: url(/static/images/left-bg.png) no-repeat left top;
+  background-size: 100% 100%;
 }
 .title {
   padding-left: 34px;
@@ -461,6 +634,7 @@ export default {
   font-size: 38px;
   font-weight: 800;
   color: #ffffff;
+  letter-spacing: 4px;
   line-height: 42px;
 }
 .sub-title {
@@ -482,10 +656,27 @@ export default {
   padding-left: 116px;
 }
 .finance-progress {
+  position: relative;
   width: 446px;
   height: 8px;
   background: #2a3242;
   border-radius: 4px;
+}
+.finance-progress-month::after,
+.finance-progress-all::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  height: 8px;
+  background-color: #4c82ff;
+  border-radius: 4px;
+}
+.finance-progress-month::after {
+  width: 52%;
+}
+.finance-progress-all::after {
+  width: 20%;
 }
 .fix-clear::before,
 .fix-clear::after {
@@ -522,32 +713,36 @@ export default {
   color: #2e7ae6;
 }
 .finance-chart {
-  height: 500px;
+  height: 400px;
 }
 .service-wrapper {
   display: flex;
+  margin-bottom: 60px;
 }
 .service-left,
 .service-right {
   flex: 1;
 }
 #security,
-#repair,
-#order {
+#repair {
   height: 288px;
 }
-.park-wrapper {
+#order {
+  height: 250px;
+}
+.income-wrapper,
+.park {
   display: flex;
-  justify-content: space-between;
 }
-.park-left {
-  width: 250px;
-  flex-basis: 250px;
-}
-.park-right {
-  flex: 1;
+.park {
+  align-items: flex-end;
 }
 .income {
+  width: 248px;
+  margin-right: 45px;
+}
+.income-all,
+.income-today {
   height: 164px;
   margin-bottom: 42px;
 }
@@ -574,51 +769,26 @@ export default {
   line-height: 32px;
 }
 .little-title {
-  font-size: 14px;
-  font-weight: 500;
   color: #abb7cc;
-  line-height: 14px;
+}
+.park-space {
+  flex: 1;
+  margin-left: 75px;
 }
 #park-space {
-  height: 288px;
+  width: 248px;
+  height: 257px;
 }
 #income {
+  flex: 1;
   height: 320px;
 }
 .flow-progress {
+  position: relative;
   height: 8px;
+  margin-top: 24px;
   background: #152336;
   border-radius: 4px;
-}
-.txt-font14 {
-  font-size: 14px;
-  font-weight: 400;
-  color: #abb7cc;
-  line-height: 14px;
-}
-.txt-font52 {
-  font-size: 52px;
-  font-weight: 800;
-  color: #0fe4f8;
-  line-height: 52px;
-}
-.txt-font20 {
-  font-size: 20px;
-  font-weight: 400;
-  color: #ffffff;
-  line-height: 20px;
-}
-.txt-font16 {
-  font-size: 16px;
-  font-weight: 400;
-  color: #abb7cc;
-  line-height: 16px;
-}
-.txt-font32 {
-  font-size: 32px;
-  font-weight: bold;
-  color: #ffffff;
-  line-height: 32px;
 }
 .park-title {
   padding-bottom: 22px;
@@ -632,6 +802,9 @@ export default {
   display: flex;
   flex-direction: column;
 }
+.car-flow:nth-of-type(1) {
+  margin-bottom: 44px;
+}
 .flow-today,
 .flow-average {
   align-self: flex-end;
@@ -640,4 +813,88 @@ export default {
 .flow-average span:first-child {
   margin-right: 12px;
 }
+.flow-progress-today::before,
+.flow-progress-average::before {
+  content: url(/static/images/car.png);
+  display: block;
+  position: absolute;
+  top: -20px;
+}
+.flow-progress-today::before {
+  left: 50%;
+}
+.flow-progress-average::before {
+  left: 30%;
+}
+.flow-progress-today::after,
+.flow-progress-average::after {
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  height: 8px;
+  background-color: #4c82ff;
+  border-radius: 4px;
+}
+.flow-progress-today::after {
+  width: 50%;
+}
+.flow-progress-average::after {
+  width: 30%;
+}
+.visitor {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.frame {
+  width: 428px;
+  height: 84px;
+  padding-left: 64px;
+  padding-top: 24px;
+  margin: 0 22px 60px;
+  background: url(/static/images/frame.png) no-repeat left;
+  background-size: 100% 100%;
+}
+.frame span {
+  margin-left: 28px;
+}
+.user,
+.gate {
+  flex: 1;
+}
+#user {
+  height: 300px;
+}
+.gate-wrapper {
+  width: 538px;
+  height: 297px;
+  background: url(/static/images/table.png) no-repeat left;
+  background-size: 100% 100%;
+}
+.gate-table > thead > tr {
+  height: 45px;
+  text-align: center;
+  color: #0fe4f8;
+}
+.gate-table > tbody > tr {
+  height: 60px;
+  text-align: center;
+  color: #ffffff;
+}
+.serial-num {
+  color: #abb7cc;
+}
+.fail-num {
+  color: #0fe4f8;
+}
+.device-name {
+  width: 120px;
+  max-width: 120px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  vertical-align: middle;
+}
 </style>
+
